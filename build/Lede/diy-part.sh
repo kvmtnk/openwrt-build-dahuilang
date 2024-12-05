ea8264b15e923e387755d141c245f25462aef83a
kvmtnk <31096029+kvmtnk@users.noreply.github.com>
2024-12-05 19:41:17 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -68,9 +68,10 @@ export Disable_autosamba="0"                 # 去掉源码默认自选的luci-a
 # uci set luci.main.mediaurlbase='/luci-static/neobird' && uci commit luci
  # 启动18.06Luci
 #sed -i 's@src-git luci@# src-git luci@g' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+git clone https://github.com/kenzok8/small-package/tree/main/luci-app-npc
 
 # 其他
 export Ttyd_account_free_login="0"           # 设置ttyd免密登录(1为启用命令,填0为不作修改)
