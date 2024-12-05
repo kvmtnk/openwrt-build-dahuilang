eb8a0b375eca9fbdc8962f6a816e796235d443a0
kvmtnk <31096029+kvmtnk@users.noreply.github.com>
2024-12-05 19:59:49 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -71,7 +71,7 @@ export Disable_autosamba="0"                 # 去掉源码默认自选的luci-a
 
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
-git clone https://github.com/kenzok8/small-package/tree/main/luci-app-npc
+
 
 # 其他
 export Ttyd_account_free_login="0"           # 设置ttyd免密登录(1为启用命令,填0为不作修改)
